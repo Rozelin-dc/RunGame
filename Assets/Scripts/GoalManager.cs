c3e22ba3305ddc86aec9dcf68f76aa8c99d5fe5d
Rozelin <ikeda.m.aj@m.titech.ac.jp>
2020-10-17 22:27:33 +0900

    progress(10/17)

---
+++ Assets/Scripts/GoalManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoalManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject goalText;
+    private bool isGoal = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGoal && Input.GetMouseButton(0))
+        {
+            Restart();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == player.name)
+        {
+            goalText.GetComponent<Text>().text = "Goal!\n画面クリックで再スタート";
+            goalText.SetActive(true);
+            isGoal = true;
+        }
+    }
+
+    private void Restart()
+    {
+        Scene loadScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadScene.name);
+    }
+}
